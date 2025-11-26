78b247b7ef49164ac6bc0938f27efc686cdcde90
Mraksimus <maks.cucheryavenko@gmail.com>
2025-11-26 22:45:28 +0300

    fix: add Serializable annotation

--- main/src/main/kotlin/ru.psychologicalTesting.main/infrastructure/controllers/common/responses/ConflictResponse.kt
+++ main/src/main/kotlin/ru.psychologicalTesting.main/infrastructure/controllers/common/responses/ConflictResponse.kt
@@ -3,7 +3,9 @@ package ru.psychologicalTesting.main.infrastructure.controllers.common.responses
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.response.respond
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ConflictResponse(
     val errors: Map<String, String> = emptyMap(),
     val description: String? = null
